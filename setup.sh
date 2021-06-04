22fbb47a55e430008d7af4afd43fd03f557a5c71
cpeqt3330 <85354330+cpeqt3330@users.noreply.github.com>
2021-06-04 20:26:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,16 @@ export PATH=$PWD/rclone-v1.53.0-linux-amd64:$PATH
 echo "Trying to configure rclone..."
 echo -e "$RCLONE_CONFIG" > rclone.conf
 
+#install olaris
+echo "Updating olaris-server from nightlies"
+cd /opt
+curl -vL 'https://gitlab.com/api/v4/projects/olaris%2Folaris-server/jobs/artifacts/develop/download?job=dist-linux-amd64' > /opt/build.zip && unzip -o /opt/build.zip && unzip "olaris-linux-amd64-*.zip"
+echo "running olaris-server"
+chmod +x /opt/bin/olaris
+
 export OLARIS_SERVER_PORT=$PORT
 echo "PORT IS"
 echo "$PORT"
 echo $PATH > PATH
+cd /opt/bin/
+./olaris
